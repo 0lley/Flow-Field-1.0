a9ce1c7f6114c8116e9085112cb4af0c48dfb0cc
0lley <Olexandr.Kurakin@gmail.com>
2020-04-24 01:53:25 +1000

    Working on adding the trails

--- Fluid Flow/particle.js
+++ Fluid Flow/particle.js
@@ -1,7 +1,7 @@
 class Particle {
     constructor(angle, angleFix) {
-        this.posX = width / 2;
-        this.posY = height / 2;
+        this.posX = mouseX;
+        this.posY = mouseY;
         this.maxSpeed = 5;
         this.angle = angle;
         this.velX = 0;
@@ -9,19 +9,20 @@ class Particle {
         this.colInc = 0;
         this.colOff = 0.01;
         this.angleFix = angleFix;
+        this.trail = [];
     }
 
     show() {
         noStroke();
         fill((1 - noise(frameCount * 0.01)) * 255, 0, (noise(frameCount * 0.01) * 255));
         this.colInc += this.colOff;
-        circle(this.posX, this.posY, 5);
+        circle(this.posX, this.posY, particleSize.value());
     }
 
     accelerate(magniVal, zNoiseAngle, zNoiseMagnitude) { 
         let theta = noise(this.posX * magniVal, this.posY * magniVal, zNoiseAngle) * PI * this.angleFix;
-        this.velX += (noise(this.posX * (20 / width), this.posY * (20 / height), zNoiseMagnitude) * sin(theta)) * 2.5;
-        this.velY += (noise(this.posX * (20 / width), this.posY * (20 / height), zNoiseMagnitude) * cos(theta)) * 2.5;
+        this.velX += (noise(this.posX * (20 / width), this.posY * (20 / height), zNoiseMagnitude) * cos(theta)) * accelerationSlider.value() / 40;
+        this.velY += (noise(this.posX * (20 / width), this.posY * (20 / height), zNoiseMagnitude) * sin(theta)) * accelerationSlider.value() / 40;
         if (this.velX > this.maxSpeed) {
             this.velX = this.maxSpeed;
         }
@@ -39,6 +40,9 @@ class Particle {
     }
 
     move() {
+        if (trailParticle) {
+            this.trail.push('hmm yes');
+        }
         this.posX += this.velX;
         this.posY += this.velY;
     }
--- Fluid Flow/sketch.js
+++ Fluid Flow/sketch.js
@@ -1,12 +1,15 @@
-let maxSlider, creationSlider, trailBox;
+let maxSlider, creationSlider, accelerationSlider, particleSize;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(60);
   noiseDetail(2);
-  maxSlider = createSlider(10, 5000, 100);
-  creationSlider = createSlider(1, 50, 5);
-  trailBox = createCheckbox('Trails on?');
+  maxSlider = createSlider(10, 5000);
+  creationSlider = createSlider(1, 50, 1);
+  accelerationSlider = createSlider(10, 200, 1);
+  particleSize = createSlider(1, 10, 3);
+  let trailBox = createCheckbox('Trail on?', false);
+  trailBox.changed(checkBoxTrue);
 }
 
 let particles = [];
@@ -20,20 +23,28 @@ let magniVal = 1;
 let magShift = 1;
 let noiseFix = 0;
 let noiseFixOffset = 0.01;
-// let magShift = 1;
-// let zOffAng = 1;
-// let magniVal = 1;
+let trailParticle;
 
+function checkBoxTrue() {
+  if (this.checked()) {
+    trailParticle = true;
+  } else {
+    trailParticle = false;
+  }
+}
 
 function draw() {
   background(0);
-  if (particles.length < maxSlider.value()) {
-    for (let i = 0; i < creationSlider.value(); i++) {
-      particles.push(new Particle(random() * TWO_PI * 1.5, noise(noiseFix) * 15));
-      xAngle += xAngleOffset;
-      noiseFix += noiseFixOffset;
+  if (mouseIsPressed) {
+    if (particles.length < maxSlider.value()) {
+      for (let i = 0; i < creationSlider.value(); i++) {
+        particles.push(new Particle(random() * TWO_PI * 1.5, noise(noiseFix) * 15));
+        xAngle += xAngleOffset;
+        noiseFix += noiseFixOffset;
+        }
+      }
     }
-  }
+
   for (let i = 0; i < particles.length; i++) {
     if (particles[i].posCheck()) {
       particles.splice(i, 1);
@@ -47,17 +58,8 @@ function draw() {
       particles[i].show();
     }
   }
-  console.log(frameRate())
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function myCheckedEvent() {
-  if (trailBox.checked()) {
-    console.log('Checking!');
-  } else {
-    console.log('Unchecking!');
-  }
 }
